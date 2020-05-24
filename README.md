0a8966c2148e251b6f91d136ce9165092b0c88a8
Abolfazl Ghaemi <Abolixes@gmail.com>
2020-05-24 13:14:47 +0430

    initial commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# Persian Lorem
+
+Simple persian lorem ipsum generator
+
+## Install
+
+1. Open VS Code
+2. Press F1
+3. Type "install"
+4. Select "Extensions: Install Extension".
+5. Select Chinese Lorem Ipsm from the list
+
+## Usage
+
+1. Press F1 or `ctrl + shift + p` then enter 'Persian Lorem'
+2. Type how many words you want to generate ( empty for 1 paragraph )
+
+### Or
+
+In your editor , type `plorem` to generate 1 paragraph of lorem ipsum or use `plorem` with number (example `lorem20`) to generate 20 words .
---
+++ src/extension.ts
@@ -0,0 +1,104 @@
+import * as vscode from "vscode";
+import {
+  Command,
+  TriggerCharacters,
+  CommandNumberRegExp,
+  LoremText,
+} from "./const";
+
+const insertText = (LoremData: any) => {
+  var editor: any = vscode.window.activeTextEditor;
+  editor.edit((edit: any) =>
+    editor.selections.forEach((selection: any) => {
+      edit.delete(selection);
+      edit.insert(selection.start, PersianLorem(LoremData.words));
+    })
+  );
+};
+
+const PersianLorem = (Words: any) => {
+  var LoremReturn = "";
+  if (Words.length === 0 || Words === 0) {
+    return LoremText;
+  }
+  var LoremSplited = LoremText.split(" ");
+  var SentenceReset = 0;
+  for (var i = 0; i < Words; i++) {
+    if (SentenceReset === LoremSplited.length) {
+      SentenceReset = 0;
+    }
+    LoremReturn += LoremSplited[SentenceReset] + " ";
+    SentenceReset++;
+  }
+  return LoremReturn;
+};
+
+const extractNumber = (
+  document: vscode.TextDocument,
+  position: vscode.Position,
+): number => {
+  const replaceCommandFromLastGroup = (
+    matchGroup: RegExpMatchArray,
+  ): number => {
+    const lastGroup = matchGroup[matchGroup.length - 1];
+
+    if (lastGroup === Command) {
+      return 0;
+    }
+    return parseInt(lastGroup.replace(Command, ""));
+  };
+
+  const line: string = document.lineAt(position).text;
+  const matchGroup: RegExpMatchArray | null = line.match(CommandNumberRegExp);
+
+  return matchGroup !== null ? replaceCommandFromLastGroup(matchGroup) : 0;
+};
+
+export function activate(context: vscode.ExtensionContext) {
+  const AutoCompletion = vscode.languages.registerCompletionItemProvider(
+    "*",
+    {
+      provideCompletionItems(
+        document: vscode.TextDocument,
+        position: vscode.Position,
+      ) {
+        const WordCount = extractNumber(document, position);
+        const VsCommand = new vscode.CompletionItem(Command);
+        VsCommand.insertText = new vscode.SnippetString(
+          PersianLorem(WordCount),
+        );
+        VsCommand.label = `${Command}${WordCount || ""}`;
+
+        var Documentation = "";
+        WordCount === 0
+          ? (Documentation = "Generate 1 Paragraph of Persian Lorem")
+          : (Documentation = `Generate ${WordCount} Words of Persian Lorem`);
+        VsCommand.documentation = new vscode.MarkdownString(Documentation);
+        return [VsCommand];
+      },
+    },
+    ...TriggerCharacters,
+  );
+  context.subscriptions.push(AutoCompletion);
+
+  /////////////////////
+  // Command
+  /////////////////////
+
+  let disposable = vscode.commands.registerCommand(
+    "extension.GenerateLorem",
+    () => {
+      vscode.window
+        .showInputBox({
+          placeHolder: "How many words ? ( empty for 1 paragraph )",
+          prompt:
+            "Type how many words you want to insert ( empty for 1 paragraph ) ",
+        })
+        .then((words: any) => {
+          insertText({ words: words });
+        });
+    },
+  );
+  context.subscriptions.push(disposable);
+}
+export function deactivate() {}
